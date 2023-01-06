c86da0c77494d29c298eaa79cb55338ad4cf4950
Nick Dellar <ndelfern@live.ca>
2023-01-06 19:45:23 +0000

    refactor interviewerlistitem and interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -12,8 +12,7 @@ export default function InterviewerList(props) {
         name={interviewer.name} 
         avatar={interviewer.avatar} 
         selected={interviewer.id === props.interviewer}
-        setInterviewer={props.setInterviewer}
-        id={interviewer.id}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
       />
     )
   
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -12,7 +12,7 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li onClick={() => props.setInterviewer(props.id)} className={InterviewerListClassName}>
+    <li classname={InterviewerListClassName} onClick={props.setInterviewer}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
