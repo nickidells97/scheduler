62a09fcddf030be3a385345d174ea47ef560f465
Nick Dellar <ndelfern@live.ca>
2023-01-14 00:56:48 +0000

    add useVisualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,33 @@
+import { react, useState } from "react";
+
+
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(data, replace = false) {
+
+    const newHistory = [...history]
+
+    if (replace === true) {
+      newHistory.pop()
+    }
+    setMode(data)
+    newHistory.push(data);
+    setHistory(newHistory)
+  }
+
+  function back() {
+    if (history.length < 2) {
+      return;
+    }
+    const newHistory = [...history]
+    newHistory.pop()
+    setHistory(newHistory);
+    setMode(newHistory[newHistory.length - 1])
+  }
+
+  return { mode, transition, back };
+}
+
