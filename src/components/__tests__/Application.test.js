373c12699dff9c8e7c62d1744174d426560a82e1
Nick Dellar <ndelfern@live.ca>
2023-01-18 20:01:42 +0000

    refactor __test__ folder files

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -4,8 +4,18 @@ import { render, cleanup } from "@testing-library/react";
 
 import Application from "components/Application";
 
+import { waitForElement } from "@testing-library/react";
+
+import { fireEvent } from "@testing-library/react";
+
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
